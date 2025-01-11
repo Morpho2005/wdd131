176c69b2d9588b2079a593a0ec350029c8e458a0
morpho2005 <digitalchristian05@gmail.com>
2025-01-11 11:55:32 -0700

    commit

--- scripts/getdate.js
+++ scripts/getdate.js
@@ -1,16 +1,7 @@
 // select the DOM elements for output
-const full = document.querySelector("#full");
-const worldfull = document.querySelector("#world-full");
-const short = document.querySelector("#short");
-const medium = document.querySelector("#medium");
 const year = document.querySelector("#year");
 
 // use the date object
 const today = new Date();
-
-full.innerHTML = ``;
-worldfull.innerHTML = ``;
-short.innerHTML = ``;
-medium.innerHTML = ``;
 
 year.innerHTML = `<span>${today.getFullYear()}</span>`;
