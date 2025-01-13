2c8d9a9ef962190fa69a8a6b858f290be0a61e5c
morpho2005 <digitalchristian05@gmail.com>
2025-01-13 10:52:24 -0700

    commit

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -5,7 +5,18 @@ const list = document.querySelector('______'); // you need to fill in the blank 
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') { 
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+        input.focus();
+    }
+});
