0705b547cecb4e99133aedcf4ecba5c3e91b3644
morpho2005 <digitalchristian05@gmail.com>
2025-01-18 14:00:21 -0700

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,9 @@
 // select the DOM elements for output
 const year = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified")
 
 // use the date object
 const today = new Date();
 
-year.innerHTML = `<span>${today.getFullYear()}</span>`;
+year.innerHTML = today.getFullYear();
+lastModified.innerHTML = document.lastModified;
